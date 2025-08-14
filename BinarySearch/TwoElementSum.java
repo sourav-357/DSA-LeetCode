2fef46b7abe0f51681738a6c268357ac5a21ede0
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-14 20:32:39 +0530

    two element approach

---
+++ BinarySearch/TwoElementSum.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+public class TwoElementSum {
+
+}
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1}; // 1-indexed
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1}; // if no solution found 
+    }
+}
