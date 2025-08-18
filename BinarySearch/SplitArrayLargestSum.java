f82021ccb361b5dc99b6c2603c606c267fe7c80c
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-18 19:18:36 +0530

    Split array largest Sum

---
+++ BinarySearch/SplitArrayLargestSum.java
@@ -0,0 +1,42 @@
+package BinarySearch;
+
+public class SplitArrayLargestSum {
+    
+}
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int low = 0, high = 0, ans = -1; // Declaring Constraints
+        for (int i = 0; i < nums.length; i++) {
+            high += nums[i]; // Setting up High as the Sum of all the elements 
+            if (low < nums[i])
+                low = nums[i]; // Maximum elements among nums
+        }
+        // Starting the Binary Search 
+        while (low <= high) {
+            int mid = (low + high)/2; // calculating the Mid value
+            // Check for the Value of mid
+            if (check(nums, mid, k)) {
+                high = mid - 1;
+                ans = mid;
+            } else {
+                low = mid + 1; 
+            }
+        }
+        return ans;
+    }
+    // Checking if nums could be devided into K parts with max sum as mid 
+    boolean check (int[] nums, int mid, int k) {
+        int sum = 0; // Declaring Constraints
+        int count = 1; // Starting from the first Value
+        for (int i = 0; i < nums.length; i++) {
+            if (sum + nums[i] > mid) {
+                sum = nums[i];
+                count++;
+            } else {
+                sum += nums[i];
+            }
+        }
+        // Check if it could be devided into K parts or not 
+        return (count <= k);
+    }
+}
