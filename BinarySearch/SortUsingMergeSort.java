e3b59e63d9f788ed37ddf35f01f58b8318833918
Sourav Kumar <xsouravkumar357@gmail.com>
2025-08-21 22:44:19 +0530

    Sort Using merge Sort

--- BinarySearch/SortUsingMergeSort.java
+++ BinarySearch/SortUsingMergeSort.java
@@ -1,3 +1,10 @@
+/*
+Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity 
+and with the smallest space complexity possible.
+*/
+
 package BinarySearch;
 
 class SortUsingMergeSort {
